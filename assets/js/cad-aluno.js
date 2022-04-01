4950b9d96535e4c938dfb34f340ac64bc2a3ecb5
PabloKauan-TI <kpablo429@gmail.com>
2022-04-01 13:39:28 -0300

    atualização

---
+++ assets/js/cad-aluno.js
@@ -0,0 +1,13 @@
+function cadaluno() {
+    
+     const nome = document.getElementById("nome_aluno").value
+     const curso = document.getElementById("curso_aluno").value
+     const serie = document.getElementById("serie_aluno").value
+     const matricula = document.getElementById("matricula_aluno").value
+     const telefone = document.getElementById("telefone_aluno").value
+     const endereco = document.getElementById("endereco_aluno").value
+     const email = document.getElementById("email_aluno").value
+     const senha = document.getElementById("senha_aluno").value
+
+     
+}
