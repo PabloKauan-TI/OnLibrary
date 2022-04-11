01856538059b037b84d9853e7064a62693cd4a47
PabloKauan-TI <kpablo429@gmail.com>
2022-04-11 16:39:20 -0300

    cadastro finalizado com função

--- cad_aluno.php
+++ cad_aluno.php
@@ -15,15 +15,13 @@
     <nav class="navbar navbar-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="" id="logo"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Livros
                         </a>
                         <ul class="dropdown-menu  dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
@@ -37,8 +35,7 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Alunos
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
@@ -51,8 +48,7 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Admins
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
@@ -69,21 +65,45 @@
         <div class="container">
             <fieldset>
                 <legend>Cadastrar Usuário</legend>
-                <form action="" method="post">
+                <?php
+
+                require_once 'assets/php/config.php';
+                if ($_POST) {
+                    $nome = $_POST["nome_aluno"];
+                    $curso = $_POST["curso_aluno"];
+                    $serie = $_POST["serie_aluno"];
+                    $matricula = $_POST["matricula_aluno"];
+                    $telefone = $_POST["telefone_aluno"];
+                    $endereco = $_POST["endereco_aluno"];
+                    $email = $_POST["email_aluno"];
+                    $senha = $_POST["senha_aluno"];
+
+                    $sql = "INSERT INTO cadaluno (matricula_aluno,nome_aluno, email_aluno, senha_aluno, telefone_aluno, endereco_aluno, curso_aluno, serie_aluno) VALUES('$matricula', '$nome', '$email', '$senha', '$telefone', '$endereco', '$curso', '$serie')";
+
+                    if ($con->query($sql) === TRUE) {
+                        echo "Novo cadastro realizado com sucesso! <br>"; 
+                    } else {
+                        echo "Erro" . $sql . ' ' . $con->connect_error;
+                    }
+
+                    $con->close();
+                }
+                ?>
+                <form action="cad_aluno.php" method="post">
                     <div class="row">
                         <div class="col-9">
                             <label for="nome_aluno" class="form-label">Nome</label>
-                            <input type="text" id="nome_aluno" class="form-control" placeholder="">
+                            <input type="text" name="nome_aluno" class="form-control" placeholder="">
                         </div>
                         <div class="col-3">
                             <label for="matricula_aluno" class="form-label">Matrícula</label>
-                            <input type="text" id="matricula_aluno" class="form-control" placeholder="">
+                            <input type="text" name="matricula_aluno" class="form-control" placeholder="">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
                             <label for="serie_aluno" class="form-label">Série</label>
-                            <select id="serie_aluno" class="form-select">
+                            <select name="serie_aluno" class="form-select">
                                 <option selected></option>
                                 <option value="1º">1º</option>
                                 <option value="2º">2º</option>
@@ -92,7 +112,7 @@
                         </div>
                         <div class="col">
                             <label for="curso_aluno" class="form-label">Curso</label>
-                            <select id="curso_aluno" class="form-select">
+                            <select name="curso_aluno" class="form-select">
                                 <option selected></option>
                                 <option value="Enfermagem">Enfermagem</option>
                                 <option value="Informática">Informática</option>
@@ -104,30 +124,28 @@
                     <div class="row">
                         <div class="col">
                             <label for="endereco_aluno" class="form-label">Endereço</label>
-                            <input type="text" id="endereco_aluno" class="form-control" placeholder="">
+                            <input type="text" name="endereco_aluno" class="form-control" placeholder="">
                         </div>
                         <div class="col">
                             <label for="telefone_aluno" class="form-label">Telefone</label>
-                            <input type="text" id="telefone_aluno" class="form-control" placeholder="">
+                            <input type="text" name="telefone_aluno" class="form-control" placeholder="">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
                             <label for="email_aluno" class="form-label">E-mail</label>
-                            <input type="email" id="email_aluno" class="form-control" placeholder="">
+                            <input type="email" name="email_aluno" class="form-control" placeholder="">
                         </div>
                         <div class="col">
                             <label for="senha_aluno" class="form-label">Senha</label>
-                            <input type="password" id="senha_aluno" class="form-control" placeholder="">
+                            <input type="password" name="senha_aluno" class="form-control" placeholder="">
                         </div>
                     </div>
-                    <button type="submit" action="cadaluno()">Cadastrar</button>
+                    <button type="submit">Cadastrar</button>
                 </form>
             </fieldset>
         </div>
     </div>
-
-    <script src="assets/js/cad-aluno.js"></script>
 </body>
 
 </html>
