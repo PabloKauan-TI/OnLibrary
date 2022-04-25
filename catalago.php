f21c208079a03f797b10615061cf80fb33111598
PabloKauan-TI <kpablo429@gmail.com>
2022-04-25 16:24:06 -0300

    input ARQUIVO

--- catalago.php
+++ catalago.php
@@ -64,60 +64,8 @@
     <div class="form">
         <div class="container">
             <fieldset>
-                <legend>Cadastrar Livro</legend>
-                <?php
-
-                require_once 'assets/php/config.php';
-                if ($_POST) {
-                    $nome = $_POST["nome_livro"];
-                    $curso = $_POST["curso_aluno"];
-                    $serie = $_POST["serie_aluno"];
-                    $matricula = $_POST["codigo_livro"];
-                    $telefone = $_POST["telefone_aluno"];
-                    $endereco = $_POST["endereco_aluno"];
-                    $email = $_POST["email_aluno"];
-                    $senha = $_POST["senha_aluno"];
-
-                    $sql = "INSERT INTO cadaluno (codigo_livro,nome_livro, email_aluno, senha_aluno, telefone_aluno, endereco_aluno, curso_aluno, serie_aluno) VALUES('$matricula', '$nome', '$email', '$senha', '$telefone', '$endereco', '$curso', '$serie')";
-
-                    if ($con->query($sql) === TRUE) {
-                        echo "Novo cadastro realizado com sucesso! <br>";
-                    } else {
-                        echo "Erro" . $sql . ' ' . $con->connect_error;
-                    }
-
-                    $con->close();
-                }
-                ?>
-                <form action="cad_livro.php" method="post">
-                    <div class="row">
-                        <div class="col-9">
-                            <label for="nome_livro" class="form-label">Nome</label>
-                            <input type="text" name="nome_livro" class="form-control" placeholder="">
-                        </div>
-                        <div class="col-3">
-                            <label for="codigo_livro" class="form-label">Código</label>
-                            <input type="text" name="codigo_livro" class="form-control" placeholder="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-9">
-                            <label for="autor_livro" class="form-label">Autor</label>
-                            <input type="text" name="autor_livro" class="form-control" placeholder="">
-                        </div>
-                        <div class="col-3">
-                            <label for="qtde_livro" class="form-label">Quantidade</label>
-                            <input type="text" name="qtde_livro" class="form-control" placeholder="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <label for="img" class="form-label">Capa</label>
-                            <input type="file" name="img" class="form-control" placeholder="">
-                        </div>
-                    </div>
-                    <button type="submit">Cadastrar</button>
-                </form>
+                <legend>Catálogo</legend>
+                
             </fieldset>
         </div>
     </div>
