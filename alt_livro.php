b377ce56455836f44d09f619281366f1ca382ed4
PabloKauan-TI <kpablo429@gmail.com>
2022-05-20 13:13:45 -0300

    alterar com problema

--- alt_livro.php
+++ alt_livro.php
@@ -1,10 +1,11 @@
 <?php 
-    include("assets/php/config.php"); 
-    $sql = "SELECT * FROM cadlivro";
-    $result = $con->query($sql);
+	include 'assets/php/config.php';
+    $id = $_GET['cod_livro'];
+    $result = mysqli_query($con, "SELECT FROM cdalivro WHERE cod_livro = $id");
+    while($dado = mysqli_fetch_assoc($result)){
 ?>
-
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
@@ -12,10 +13,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnLibrary</title>
-    <link rel="stylesheet" href="assets/css/gec_livro.css">
+    <link rel="stylesheet" href="assets/css/cadastros.css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/fuctions.js"></script>
 </head>
 
 <body>
@@ -32,21 +32,8 @@
                             Livros
                         </a>
                         <ul class="dropdown-menu  dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="catalogo.php">Catalago</a></li>
-                            <li><a class="dropdown-item" href="cad_livro.php">Cadastrar</a></li>
-                            <li><a class="dropdown-item" href="#">Gerenciar</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Emprestimos
-                        </a>
-                        <ul class="dropdown-menu  dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="cad_emprestimolivro.php">Realizar</a></li>
+                            <li><a class="dropdown-item" href="#">Catalago</a></li>
+                            <li><a class="dropdown-item" href="#">Emprestados</a></li>
                             <li><a class="dropdown-item" href="#">Gerenciar</a></li>
                         </ul>
                     </li>
@@ -59,7 +46,7 @@
                             Alunos
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="cad_aluno.php">Cadastrar</a></li>
+                            <li><a class="dropdown-item" href="#">Com Livros</a></li>
                             <li><a class="dropdown-item" href="#">Gerenciar</a></li>
                         </ul>
                     </li>
@@ -72,7 +59,7 @@
                             Admins
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="cad_bibliotecaria.php">Cadastrar</a></li>
+                            <li><a class="dropdown-item" href="#">Todos</a></li>
                             <li><a class="dropdown-item" href="#">Gerenciar</a></li>
                         </ul>
                     </li>
@@ -81,29 +68,43 @@
         </div>
     </nav>
 
-    <section class="catalog" id="catalog">
-        <div class="content">
-            <div class="card-wrapper">
-                <?php while($dado = mysqli_fetch_assoc($result)){ ?>
-                <div class="card-item">
-                    <img src="./imagens/<?php echo $dado['imagem_livro']; ?>">
-                    <div class="card-content">
-                        <h3 id="titulo"><?php echo $dado["titulo_livro"];?></h3>
-                        <p>
-                            Autor: <?php echo $dado["autor_livro"]; ?><br>
-                            Quantidade: <?php echo $dado["qtd_livro"]; ?>
-                        </p>
+    <div class="form">
+        <div class="container">
+            <fieldset>
+                <legend>Cadastrar Livro</legend>
+                <form method="post" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-9">
+                            <label for="titulo_livro" class="form-label">Nome</label>
+                            <input type="text" name="titulo_livro" class="form-control" value="<?php echo $dado['titulo_livro']; ?>">
+                        </div>
+                        <div class="col-3">
+                            <label for="codigo_livro" class="form-label">Código</label>
+                            <input type="text" name="cod_livro" class="form-control" value="<?php echo $dado['cod_livro']; ?>">
+                        </div>
                     </div>
-                    <div class="card-btn">
-                        <button class="btn btn-secondary"><a href="alt_livro.php?cod_livro=<?php echo $dado['cod_livro']; ?>"><img src="assets/css/escrever.png"></a></button>
-                        <button class="btn btn-secondary"><a href="assets/php/delete_livro.php?cod_livro=<?php echo $dado['cod_livro']; ?>"><img src="assets/css/excluir.png"></a></button>
+                    <div class="row">
+                        <div class="col-9">
+                            <label for="autor_livro" class="form-label">Autor</label>
+                            <input type="text" name="autor_livro" class="form-control" value="<?php echo $dado['autor_livro']; ?>">
+                        </div>
+                        <div class="col-3">
+                            <label for="qtd_livro" class="form-label">Quantidade</label>
+                            <input type="text" name="qtd_livro" class="form-control" value="<?php echo $dado['qtd_livro']; ?>">
+                        </div>
                     </div>
-                </div>
-                <?php } ?>
-            </div>
+                    <div class="row">
+                        <div class="col">
+                            <label for="imagem_livro" class="form-label">Capa</label>
+                            <input type="file" name="imagem_livro" class="form-control" value="<?php echo $dado['imagem_livro']; ?>">
+                        </div>
+                    </div>
+                    <?php } ?>
+                    <button type="post" name="submit">Cadastrar</button>
+                </form>
+            </fieldset>
         </div>
-    </section>
-
+    </div>
 </body>
 
 </html>
