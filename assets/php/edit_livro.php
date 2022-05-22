c6b73bcdb9ec130a7c8c87ccf22b333de977393c
EmanoelVitor <emanuelvitor575@email.com>
2022-05-22 05:05:49 -0300

    sem erro, mas não carrega imagem editada

--- assets/php/edit_livro.php
+++ assets/php/edit_livro.php
@@ -10,14 +10,21 @@ if($_POST) {
 	$extensao = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
 
 	$novo_nome = md5(time()).".".$extensao;
-	$diretorio = "./imagens/"; 
+	$diretorio = "./imagens"; 
 
-	move_uploaded_file($_FILES['imagem_livro']['tmp_name'], $diretorio.$novo_nome);
+	$cod_livro = $_POST['cod_livro'];
+
+
 
 	$sql  = "UPDATE cadlivro SET titulo_livro = '$titulo_livro', autor_livro = '$autor_livro', qtd_livro = '$qtd_livro', imagem_livro = '$novo_nome' WHERE cod_livro = {$cod_livro}";
 	if($con->query($sql) === TRUE) {
+
+		if (move_uploaded_file($_FILES["imagem_livro"]["tmp_name"], $diretorio . $novo_nome)) {
+			if(unlink("./imagens$novo_nome")){
         echo "<script>alert('Dados atualizados com sucesso');location='/onlibrary/gec_livro.php'</script>";
 		//header('location: /onlibrary/gec_livro.php');
+		
+			}
 	} else {
 		echo "Erro : ". $con->error;
 	}
@@ -25,5 +32,4 @@ if($_POST) {
 	$con->close();
 
 }
-
-?>
+}
