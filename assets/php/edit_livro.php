4825f903fdc5f4faea2bd53c227d0bf8cb5e561f
RafaelAlvesRodrigues-TI <rafaelrodriguessss941@gmail.com>
2022-05-23 16:43:18 -0300

    editando

--- assets/php/edit_livro.php
+++ assets/php/edit_livro.php
@@ -10,7 +10,16 @@ if($_POST) {
 	
 	$cod_livro = $_POST['cod_livro'];
 
-	$sql  = "UPDATE cadlivro SET titulo_livro = '$titulo_livro', autor_livro = '$autor_livro', qtd_livro = '$qtd_livro', imagem_livro = '$imagem_livro' WHERE cod_livro = {$cod_livro}";
+	$arquivo = $_FILES['imagem_livro']['name'];
+	$extensao = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
+
+	$novo_nome = md5(time()).".".$extensao;
+
+	$diretorio = "./imagens/"; 
+
+	move_uploaded_file($_FILES['imagem_livro']['tmp_name'], $diretorio.$novo_nome);
+
+	$sql  = "UPDATE cadlivro SET titulo_livro = '$titulo_livro', autor_livro = '$autor_livro', qtd_livro = '$qtd_livro', imagem_livro = '$novo_nome' WHERE cod_livro = {$cod_livro}";
 	if($con->query($sql) === TRUE) {
         echo "<script>alert('Dados atualizados com sucesso');location='/onlibrary/gec_livro.php'</script>";
 		//header('location: /onlibrary/gec_livro.php');
