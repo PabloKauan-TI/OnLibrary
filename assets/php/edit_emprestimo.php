2cb161011d3379f06393b002256f2ccce677280d
RafaelAlvesRodrigues-TI <rafaelrodriguessss941@gmail.com>
2022-06-13 16:18:43 -0300

    edit emprestimo

---
+++ assets/php/edit_emprestimo.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once 'config.php';
+
+if($_POST) {
+	$titulo_livro = $_POST["titulo_livro"];
+                    $aluno = $_POST["aluno"];
+                    $serie = $_POST["serie"];
+                    $curso = $_POST["curso"];
+                    $data_emp = $_POST["data_emp"];
+                    $data_rec = $_POST["data_rec"];
+	
+	$sql  = "UPDATE emprestimolivro SET titulo_livro = '$titulo_livro', aluno = '$aluno', curso = '$curso', data_emp = '$data_emp', data_rec = '$data_rec' WHERE cod_emp = {$cod_emp}";
+	
+	if($con->query($sql) === TRUE) {
+
+		
+  
+		header('location: /onlibrary/gec_emp.php');
+		
+			
+	} else {
+		echo "Erro : ". $con->error;
+	}
+
+	$con->close();
+
+}
+
